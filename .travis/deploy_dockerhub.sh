9e855237a84f8064a096682e399d5e8ab6168fe4
Axel Sirota <asirota@asapp.com>
2019-10-27 16:24:43 -0300

    setting dockerhub

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker login --username ${DOCKER_USER} --password ${DOCKER_PASS}
 docker build -f Dockerfile -t ${TRAVIS_REPO_SLUG}:latest .
-docker tag ${TRAVIS_REPO_SLUG} ${DOCKER_REPO}
-docker push ${DOCKER_REPO}
+docker tag ${TRAVIS_REPO_SLUG}:latest ${DOCKER_REPO}:latest
+docker push ${DOCKER_REPO}:latest
