106bf15eaf84427be1cc4dc1febd6c7e0240b242
Axel Sirota <asirota@asapp.com>
2019-10-27 16:11:30 -0300

    removing thingys

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 docker login --username ${DOCKER_USER} --password ${DOCKER_PASS}
-TAG=${TRAVIS_TAG}
-docker build -f Dockerfile -t ${TRAVIS_REPO_SLUG}:${TAG} -t ${TRAVIS_REPO_SLUG}:latest .
+docker build -f Dockerfile -t ${TRAVIS_REPO_SLUG}:latest .
 docker tag ${TRAVIS_REPO_SLUG} ${DOCKER_REPO}
 docker push ${DOCKER_REPO}
