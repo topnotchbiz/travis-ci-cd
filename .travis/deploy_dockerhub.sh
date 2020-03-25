f79a04d351507569f121fe106a9c649b4223235f
topnotchbiz <jin.yin1229@outlook.com>
2020-03-25 18:09:17 +0800

    update sh script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-a=10
-b=20
 
-if [ $a == $b ]
-then
-   echo "a is equal to b"
-else
-   echo "a is not equal to b"
-fi
 #if [ "${DOCKER_USER}" == "topnotchbiz" ]
 #then
 #    echo "yes"
