bf3a1b7f2bb2f7085e49aa0d5e7f662d14e524e1
topnotchbiz <jin.yin1229@outlook.com>
2020-03-25 18:37:48 +0800

    update sh script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo "Usage: "
-echo "  network.sh <Mode> [Flags]"
-#docker login --username ${DOCKER_USER} --password ${DOCKER_PASS}
-#docker build -f Dockerfile -t ${TRAVIS_REPO_SLUG}:latest .
-#docker tag ${TRAVIS_REPO_SLUG}:latest ${DOCKER_REPO}:latest
-#docker push ${DOCKER_REPO}:latest
+docker login --username ${DOCKER_USER} --password ${DOCKER_PASS}
+docker build -f Dockerfile -t ${TRAVIS_REPO_SLUG}:latest .
+docker tag ${TRAVIS_REPO_SLUG}:latest ${DOCKER_REPO}:latest
+docker push ${DOCKER_REPO}:latest
