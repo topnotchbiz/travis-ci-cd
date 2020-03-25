b7c402f3d1200e93f147791d7c39ed25a93f6e8a
topnotchbiz <jin.yin1229@outlook.com>
2020-03-25 17:55:45 +0800

    update sh script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-echo ${DOCKER_USER}
-echo ${DOCKER_PASS}
+if [ "${DOCKER_USER}" == "topnotchbiz" ]; then
+    echo ${DOCKER_USER}
+else
+    echo ${DOCKER_PASS}
+ fi
 #docker login --username ${DOCKER_USER} --password ${DOCKER_PASS}
 #docker build -f Dockerfile -t ${TRAVIS_REPO_SLUG}:latest .
 #docker tag ${TRAVIS_REPO_SLUG}:latest ${DOCKER_REPO}:latest
