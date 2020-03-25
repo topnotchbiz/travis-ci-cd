39bce4bfcfe1952b861adb6deb400adf977c2e84
topnotchbiz <jin.yin1229@outlook.com>
2020-03-25 17:47:55 +0800

    update sh script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 docker login --username ${DOCKER_USER} --password ${DOCKER_PASS}
 docker build -f Dockerfile -t ${TRAVIS_REPO_SLUG}:latest .
 docker tag ${TRAVIS_REPO_SLUG}:latest ${DOCKER_REPO}:latest
